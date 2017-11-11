7e18b1ccdb579e23d1ff133093811f5de504c0a1
StupidClyde <33563512+StupidClyde@users.noreply.github.com>
2017-11-10 22:27:39 -0800

    Added Spasy the ghost

--- server/ai.cpp
+++ server/ai.cpp
@@ -31,3 +31,28 @@ Direction Ai::clyde(std::vector<int> map, int pacPosition, int clydePosition, Di
         return clydeDir;
     }
 }
+
+Direction Ai::spasy(std::vector<int> map, int pacPosition, int clydePosition, Direction clydeDir)
+{
+    std::vector<Direction> validDirections;
+    if !(isWall(getAboveTile(clydePosition))){
+        validDirections.push_back(Direction::UP);
+    }
+    if !(isWall(getRightTile(clydePosition))){
+        validDirections.push_back(Direction::RIGHT);
+    }
+    if !(isWall(getBottomTile(clydePosition))){
+        validDirections.push_back(Direction::DOWN);
+    }     
+    if !(isWall(getLeftTile(clydePosition))){
+        validDirections.push_back(Direction::LEFT);
+    }
+    int s = validDirections.size();
+    if (s>1){
+        int index = rand()%s;
+        return validDirections[index];
+    }
+    else {
+        return clydeDir;
+    } 
+}
