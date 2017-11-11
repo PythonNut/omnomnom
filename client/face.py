003b825fc62f5942fb69fd2f62adc5d01e146b03
PythonNut <PythonNut@users.noreply.github.com>
2017-11-11 05:53:13 -0800

    Add updates to face recognition to prepare for integration

--- client/face.py
+++ client/face.py
@@ -4,18 +4,21 @@ import numpy as np
 import math
 import time
 import cv2
+import socket_lib
+
+# S = socket_lib.ClientSocket()
 
 video_capture = cv2.VideoCapture(0)
 
 face_locations = []
 face_names = []
 process_this_frame = 0
+oc_time = 0
+oc_debounced = False
 nom = False
 
 up_mark = None
 down_mark = None
-ud = 'center'
-lr = "center"
 
 while True:
     ret, frame = video_capture.read()
@@ -53,25 +56,15 @@ while True:
         jaw_tips_x = (face_landmarks[0]['chin'][0][0] + face_landmarks[0]['chin'][-1][0])/2
         jaw_tips_y = (face_landmarks[0]['chin'][0][1] + face_landmarks[0]['chin'][-1][1])/2
 
-        oc_metric = math.sqrt((top_lip_x - bot_lip_x)**2 + (top_lip_y - bot_lip_y)**2)/face_sd
-        oc = 'closed'
-        if oc_metric >= 0.12:
-            oc = 'open'
-            # if not nom:
-            #     print("nom")
-            nom = True
-        else:
-            nom = False
-            # print("closed")
-            # print(d)
-
         lr_metric = (nose_x - face_x)/face_sd
+        lr = "center"
         if lr_metric > 0.3:
             lr = "left"
         if lr_metric < -0.3:
             lr = "right"
 
         ud_metric = (nose_y - face_y)/face_sd
+        ud = 'center'
         if up_mark and down_mark:
             # print("ha", up_mark, down_mark)
             if ud_metric < up_mark + (down_mark - up_mark) * 0.3:
@@ -84,18 +77,53 @@ while True:
             if ud_metric > 1.0:
                 ud = "down"
 
+
+        oc_metric = math.sqrt((top_lip_x - bot_lip_x)**2 + (top_lip_y - bot_lip_y)**2)/face_sd
+        oc = 'closed'
+        if oc_metric >= 0.2:
+            oc = 'open'
+
+            # print("closed")
+            # print(d)
+        # print(face_sd)
+
+        if oc != oc_debounced:
+            if time.clock() - oc_time > 0.05:
+                oc_debounced = oc;
+                oc_time = time.clock()
+        else:
+            oc_time = time.clock()
+
+        if oc_debounced == 'closed':
+            if not nom:
+                if ud == "up":
+                    print("up" + ' '*50)
+                    # S.sendUp()
+                elif ud == "down":
+                    print("down"+ ' '*50)
+                    # S.sendDown()
+                elif lr == "left":
+                    print("left"+ ' '*50)
+                    # S.sendLeft()
+                elif lr == "right":
+                    print("right"+ ' '*50)
+                    # S.sendRight()
+            nom = True
+        else:
+            nom = False
+
+            
+        cv2.circle(frame, (nose_x*4, nose_y*4), 10, color=(0,0,255), thickness=4)
+        cv2.circle(frame, (top_lip_x*4, top_lip_y*4), 10, color=(0,0,255), thickness=4)
+        cv2.circle(frame, (bot_lip_x*4, bot_lip_y*4), 10, color=(0,0,255), thickness=4)
+
         print(lr.ljust(7),
               ud.ljust(7),
-              oc.ljust(7),
+              oc_debounced.ljust(7),
               "{0: >6.3f}".format(lr_metric),
               "{0: >6.3f}".format(ud_metric),
               "{0: >6.3f}".format(oc_metric),
               end="\r")
-
-        # print(face_sd)
-        cv2.circle(frame, (nose_x*4, nose_y*4), 10, color=(0,0,255), thickness=4)
-        cv2.circle(frame, (top_lip_x*4, top_lip_y*4), 10, color=(0,0,255), thickness=4)
-        cv2.circle(frame, (bot_lip_x*4, bot_lip_y*4), 10, color=(0,0,255), thickness=4)
 
     # Display the resulting image
     cv2.imshow('Video', frame)
@@ -110,5 +138,6 @@ while True:
         down_mark = ud_metric
 
 # Release handle to the webcam
+# del S
 video_capture.release()
 cv2.destroyAllWindows()
