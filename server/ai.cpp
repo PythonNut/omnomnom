0ae3fed0636b6cae242835cadb6d1cd07a1d47f0
StupidClyde <33563512+StupidClyde@users.noreply.github.com>
2017-11-10 22:25:01 -0800

    Add files via upload

---
+++ server/ai.cpp
@@ -0,0 +1,33 @@
+
+#include "ai.hpp"
+
+
+Ai::Ai()
+{
+    //Nothing to Do
+}
+
+Direction Ai::clyde(std::vector<int> map, int pacPosition, int clydePosition, Direction clydeDir)
+{   
+    std::vector<Direction> validDirections;
+    if !(isWall(getAboveTile(clydePosition))){
+        validDirections.push_back(Direction::UP);
+    }
+    if !(isWall(getRightTile(clydePosition))){
+        validDirections.push_back(Direction::RIGHT);
+    }
+    if !(isWall(getBottomTile(clydePosition))){
+        validDirections.push_back(Direction::DOWN);
+    }     
+    if !(isWall(getLeftTile(clydePosition))){
+        validDirections.push_back(Direction::LEFT);
+    }
+    int s = validDirections.size();
+    if (s>2){
+        int index = rand()%s;
+        return validDirections[index];
+    }
+    else {
+        return clydeDir;
+    }
+}
---
+++ server/ai.hpp
@@ -0,0 +1,17 @@
+
+
+
+#pragma once
+
+#include "helper.hpp"
+#include <stdlib.h>
+
+class ai
+{
+public:
+    ai();
+
+    Direction clyde(std::vector<int> map, int pacPosition, int clydePosition, Direction clydeDir);
+private:
+
+}
